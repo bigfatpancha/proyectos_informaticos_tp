e9b9d4c2b2f527849bcccba09b32d7b50e1a82a7
Rodrigo Zapico <rodri.zapico@gmail.com>
2018-11-05 17:10:22 -0300

    made it so that the /schedule/:doctor_id endpoint returns a list of appointments for the day. For now, all appear as available

--- turnero_api/routes/schedule.js
+++ turnero_api/routes/schedule.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var async = require('async');
+var moment = require('moment');
 
+// Returns the list of appointments for a given doctor. If a date is passed
+// by query string in the format 'DD-MM-YYYY', it'll return appointments for 
+// that date. Else, it'll default to returning for the current day.
 router.get('/:doctor_id', function(req, res) {
-  var requiredDate = new Date();
+  var requiredDate = moment();
   if (req.query.date) {
-    var dateInfo = req.query.date.split('-');
-    requiredDate = new Date(dateInfo[2], dateInfo[1] - 1, dateInfo[0]);
+    requiredDate = moment(req.query.date, "DD-MM-YYYY");
   }
-  var dayOfWeek = requiredDate.getDay();
+  var dayOfWeek = requiredDate.day();
+
   async.waterfall([
     function(callback) {
-      req.db.WorkingHours.findAll({
+      req.db.WorkingHours.findOne({
         where: {
           doctor_id: req.params.doctor_id,
           day_of_week: dayOfWeek
@@ -25,7 +29,24 @@ router.get('/:doctor_id', function(req, res) {
       });
     }, 
     function(workingHours, callback) {
-      callback(null, workingHours);
+      var appointmentStartTime = requiredDate.clone();
+      appointmentStartTime.hours(workingHours.from_hour).minutes(0);
+      var workHoursEndTime = requiredDate.clone();
+      workHoursEndTime.hours(workingHours.to_hour).minutes(0);
+
+      var appointments = [];
+      while (appointmentStartTime.isBefore(workHoursEndTime)) {
+        var appointmentEndTime = appointmentStartTime.clone();
+        appointmentEndTime.add(workingHours.appointment_duration, "minutes");
+        var appointment = {
+          start_time: appointmentStartTime.toDate(),
+          end_time: appointmentEndTime.toDate(),
+          available: true
+        }
+        appointments.push(appointment);
+        appointmentStartTime = appointmentEndTime;
+      }
+      callback(null, appointments);
     }
   ], function (err, schedule) {
     var response = {};
