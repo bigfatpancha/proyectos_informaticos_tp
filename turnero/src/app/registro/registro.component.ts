fb2c16cc39a8972ba22867fb00a817bcb61926a9
Kaoru Heanna <kaoru.heanna@trocafone.com>
2018-11-10 16:59:32 -0300

    validate regiter form

--- turnero/src/app/registro/registro.component.ts
+++ turnero/src/app/registro/registro.component.ts
@@ -7,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent {
 
+  public fields:object = {
+    email: '',
+    name: '',
+    surname: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  public errorMessage = '';
+
   constructor() { }
 
-  registrar() {
-  	
-  }
+  public registrar = () => {
+  	this.errorMessage = '';
+  	console.log("fields:",this.fields);
+  	if (!this.isValidForm()){
+  	  return;
+    }
+    this.errorMessage = "formulario valido";
+  };
+
+  private isValidForm = ():boolean => {
+    const anyMissing = Object.keys(this.fields).some( (key) => {
+      const value = this.fields[key];
+      return ((value == null) || (value === ''));
+    });
+    if (anyMissing){
+      this.errorMessage = 'Todos los campos son obligatorios';
+      return false;
+    }
+    if (this.fields.password != this.fields.confirmPassword){
+      this.errorMessage = 'Las contraseñas no coinciden';
+      return false;
+    }
+    if (!this.isSecurePassword(this.fields.password)){
+      this.errorMessage = 'La contraseña tiene que tener mínimo 8 caracteres, un numero, y una letra';
+      return false;
+    }
+    return true;
+  };
+
+  private isSecurePassword = (password:string):boolean => {
+    return (password.length >= 8) && this.hasNumber(password) && this.hasLetter(password);
+  };
+
+  private hasNumber = (myString: string):boolean =>  {
+    return /\d/.test(myString);
+  };
+
+  private hasLetter = (myString: string):boolean =>  {
+    return /[A-Za-z]/.test(myString);
+  };
 
 }
