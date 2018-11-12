012acfa7a5fcd052c78a53507040d98ba65c4763
Rodrigo Zapico <rodri.zapico@gmail.com>
2018-11-12 16:06:45 -0300

    added get and put endpoints for appointment

--- turnero_api/routes/appointment.js
+++ turnero_api/routes/appointment.js
@@ -52,4 +52,105 @@ router.post('/', function (req, res, next) {
   });
 })
 
+router.get('/:user_id', function(req, res, next) {
+  async.waterfall([
+    function(callback) {
+      req.db.Appointment.findAll({
+        where: {
+          user_id: req.params.user_id
+        }
+      })
+      .then(function(appointments) {
+        callback(null, appointments);
+      })
+      .catch(function(err) {
+        callback('internal server error');
+      });
+    }
+  ], function (err, appointments) {
+    var response = {};
+    if (!err) {
+      response.success = true;
+      response.appointments = appointments;
+    } else {
+      response.success = false;
+      response.error = err;
+    }
+    res.json(response);
+  });
+});
+
+router.put('/:user_id', function(req, res, next) {
+  async.waterfall([
+    function(callback) {
+      // Si estoy cambiando el estado a algo distinto de 'Activo',
+      // no tengo problema.
+      if (req.body.state != "Activo") {
+        return callback(null);
+      }
+
+      // Si el nuevo estado es 'Activo', valido que no haya 
+      // ningún appointment ya activo que se superponga.
+      req.db.Appointment.findOne({
+        where: {
+          doctor_id: req.body.doctor_id,
+          date: req.body.date,
+          state: 'Activo'
+        }
+      })
+      .then(function(appointment) {
+        if (!appointment || appointment.user_id == req.params.user_id) {
+          callback(null);
+        } else {
+          callback('appointment already taken');
+        }
+      })
+      .catch(function(err) {
+        callback('internal server error');
+      });
+
+    },
+    function(callback) {
+      req.db.Appointment.findOne({
+        where: {
+          user_id: req.params.user_id,
+          doctor_id: req.body.doctor_id,
+          date: req.body.date
+        }
+      })
+      .then(function(appointment) {
+        if(appointment) {
+          callback(null, appointment);
+        } else {
+          callback('appointment does not exist');
+        }
+      })
+      .catch(function(err) {
+        callback('internal server error');
+      });
+    },
+    function (appointment, callback) {
+      appointment.state = req.body.state;
+      appointment.updatedAt = moment().toDate();
+      appointment.save()
+      .then(function () {
+        callback(null, appointment.toJSON());
+      })
+      .catch(function(err) {
+        callback('internal server error');
+      });
+    }
+  ], function (err, appointment) {
+    var response = {};
+    if (!err) {
+      response.success = true;
+      response.appointment = appointment;
+    } else {
+      response.success = false;
+      response.error = err;
+    }
+    res.json(response);
+  });
+});
+
 module.exports = router;
